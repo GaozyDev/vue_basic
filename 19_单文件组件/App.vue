3e4478056ca92d5eba093e751070ce99a7d939aa
GaozyDev <670453367@qq.com>
2022-04-19 13:47:31 +0800

    基础

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+//  引入组件
+  import School from "./School";
+  import Student from "./Student";
+
+  export default {
+    name:'App',
+    components:{
+      School,
+      Student
+    }
+  }
+</script>
